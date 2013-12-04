bd04209162c8d28972aeed18796ef463fa434d17
priyankarahac <priyankarahac@gmail.com>
2013-12-04 11:08:25 +0530

    ui changes and back key press event

--- src/com/virtusa/vrave/LoginActivity.java
+++ src/com/virtusa/vrave/LoginActivity.java
@@ -5,13 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class LoginActivity extends Activity {
-
+	private ProgressBar loginProgress;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		loginProgress = (ProgressBar) findViewById(R.id.progressBarLogin);
+		loginProgress.setVisibility(View.GONE);
 	}
 
 	@Override
@@ -22,9 +26,12 @@ public class LoginActivity extends Activity {
 	}
 	
 	public void login(View view){
+		//loginProgress.setVisibility(View.VISIBLE);
 		Intent vraveIntent = new Intent(getApplicationContext(),VRaveActivity.class);
 		startActivity(vraveIntent);
+		//loginProgress.setVisibility(View.GONE);
 		overridePendingTransition(R.anim.left_to_right,R.anim.right_to_left);
+		finish();
 	}
 
 }
