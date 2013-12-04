de92f3b2f5cdf76756aa001cc89d614977e76081
priyankarahac <priyankarahac@gmail.com>
2013-12-04 11:59:47 +0530

    minor ui changes

--- gen/com/virtusa/vrave/R.java
+++ gen/com/virtusa/vrave/R.java
@@ -31,17 +31,18 @@ public final class R {
         public static final int ic_action_send_now=0x7f020003;
         public static final int ic_action_settings=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int locked=0x7f020006;
-        public static final int login_button=0x7f020007;
-        public static final int rave=0x7f020008;
-        public static final int rockstar=0x7f020009;
-        public static final int search_all_search_box_back=0x7f02000a;
-        public static final int sendthank=0x7f02000b;
-        public static final int splashscreen=0x7f02000c;
-        public static final int up=0x7f02000d;
-        public static final int view_raves_list_item_bg=0x7f02000e;
-        public static final int vip=0x7f02000f;
-        public static final int wizardofbugs=0x7f020010;
+        public static final int ic_launcher_i=0x7f020006;
+        public static final int locked=0x7f020007;
+        public static final int login_button=0x7f020008;
+        public static final int rave=0x7f020009;
+        public static final int rockstar=0x7f02000a;
+        public static final int search_all_search_box_back=0x7f02000b;
+        public static final int sendthank=0x7f02000c;
+        public static final int splashscreen=0x7f02000d;
+        public static final int up=0x7f02000e;
+        public static final int view_raves_list_item_bg=0x7f02000f;
+        public static final int vip=0x7f020010;
+        public static final int wizardofbugs=0x7f020011;
     }
     public static final class id {
         public static final int RelativeLayoutSendRaveContentArea=0x7f09000f;
